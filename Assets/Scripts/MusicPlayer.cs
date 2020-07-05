ae93dc39cbd5faa9fcc9b8305fbf1af85e18a3f1
Simon Cuddihy <simoncuddihy@gmail.com>
2020-07-05 14:55:00 +0100

    Added Splash scene and audio script

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+public class MusicPlayer : MonoBehaviour
+{
+    private float splashScreenDelay = 360f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.anyKeyDown)
+        {
+            LoadFirstScene();
+        }
+        else
+        {
+            Invoke("LoadFirstScene", splashScreenDelay); 
+        }
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
