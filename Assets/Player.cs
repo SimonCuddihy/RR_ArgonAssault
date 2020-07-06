75dbd23cb147f62bdbb702c74ff61862bcada77b
Simon Cuddihy <simoncuddihy@gmail.com>
2020-07-06 18:26:48 +0100

    Added Translation and Rotation control methods to Player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,9 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In m/s")][SerializeField] private float moveSpeed = 20f;
+    
+    // pitch control
+    [SerializeField] float yPositionPitchFactor = -3f;
+    [SerializeField] float controlPitchFactor = -15f;
+    
+    // yaw control
+    [SerializeField] float xPositionYawFactor = 3f;
+
+    // roll control
+    [SerializeField] float controlRollFactor = -3f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToYPosition = transform.localPosition.y * yPositionPitchFactor;
+        float pitchDueToControlFactor = verticalInput * controlPitchFactor;
+        float pitch = pitchDueToYPosition + pitchDueToControlFactor;
+
+        float yaw = transform.localPosition.x * xPositionYawFactor;
+
+        float rollDueToControlFactor = horizontalInput * controlRollFactor;
+        float roll = transform.localPosition.z * rollDueToControlFactor;
         
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        // Ben's Solution
+        horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        verticalInput = CrossPlatformInputManager.GetAxis("Vertical");
+        float offset = moveSpeed * Time.deltaTime;
+        float xOffset = horizontalInput * offset;
+        float yOffset = verticalInput * offset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float restrictedXPos = Mathf.Clamp(rawXPos, -10f, 10f);
+        float restrictedYPos = Mathf.Clamp(rawYPos, -8f, 8f);
+
+        transform.localPosition = new Vector3(restrictedXPos, restrictedYPos, transform.localPosition.z);
+
+        // UNITY's Solution:
+        // todo: following solution is much simpler - possibly use this in future
+        //float horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        //float verticalInput = CrossPlatformInputManager.GetAxis("Vertical");
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * offset);
     }
 }
