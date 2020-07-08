5f243f2ff471fc7c2c024676ca01edf01c8099fe
Simon Cuddihy <simoncuddihy@gmail.com>
2020-07-08 17:23:00 +0100

    File Organising and SceneLoader script

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,13 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
-using UnityEngine.SceneManagement;
+
 
 
 public class MusicPlayer : MonoBehaviour
 {
-    private float splashScreenDelay = 360f;
-
     // Awake is called before Start
     private void Awake()
     {
@@ -21,31 +19,5 @@ public class MusicPlayer : MonoBehaviour
     void Start()
     {
         
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            LoadFirstScene();
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            LoadSplashScreen(); // TODO: audio plays again over original audio when going back to splash screen
-        }
-        else
-        {
-            Invoke("LoadFirstScene", splashScreenDelay);
-        }
-    }
-
-    void LoadSplashScreen()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private float splashScreenDelay = 360f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadFirstScene();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadSplashScreen(); // TODO: audio plays again over original audio when going back to splash screen
+        }
+        else
+        {
+            Invoke("LoadFirstScene", splashScreenDelay);
+        }
+    }
+
+    void LoadSplashScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
